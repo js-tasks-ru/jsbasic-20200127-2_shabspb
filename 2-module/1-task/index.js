2e960371dfac5a51fa9a4d326351e9173d6de668
Vasilii Shabanov <shabspb@gmail.com>
2020-02-03 16:21:14 +0300

    решение 2-module/1-task Функция sumSalary(salaries)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,27 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
-function sumSalary(salaries) {
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
 
+function sumSalary(salaries) {
+  let sum = 0;
+  for (let key in salaries) {
+    if (+salaries[key]) {
+      sum += salaries[key];
+    }
+  } return sum;
 }
+
+sumSalary(salaries);
+
+
+
+
+
