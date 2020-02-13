99584311a02af00419f83862b32c749adc146a8c
Vasilii Shabanov <shabspb@gmail.com>
2020-02-13 08:44:51 +0300

    решение 4-module/1-task Функция makeFriendsList(friends)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let ul = document.createElement('ul'); // создаём элемент ul
+
+  for(let i = 0; i < friends.length; i++) { // запускаем цикл по длине объекта в переданном аргументе
+    let li = document.createElement('li'); // создаём элемент списка li
+    li.innerHTML = `${friends.firstName}, ${friends.lastName}`; // записываем имя и фамилию из объекта в элемент li
+    ul.append(li); // добавляем элемент li после элемента ul
+  }
+  return ul; // возвращаем готовый список
 }
