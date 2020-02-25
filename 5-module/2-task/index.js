f8d176669cb491016661ca63e4767fad6e68d70e
Vasilii Shabanov <shabspb@gmail.com>
2020-02-25 19:47:00 +0300

    decision 5-module/2-task Компонент Сортируемая таблица (SortableTable)

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -18,12 +18,55 @@ function SortableTable(items) {
    * @property {Element} - обязательное свойство, которое ссылается на элемент <table>
    */
   this.el = document.createElement('table');
+  this.el.insertAdjacentHTML("afterbegin", `
+    <thead>
+      <tr>
+        <td>Name</td>
+        <td>Age</td>
+        <td>Salary</td>
+        <td>City</td>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  `);
 
+  for (let item of items) {
+    this.el.tBodies[0].insertAdjacentHTML("beforeend", `
+      <tr>
+        <td>${item.name}</td>
+        <td>${item.age}</td>
+        <td>${item.salary}</td>
+        <td>${item.city}</td>
+      </tr>
+    `);
+  }
   /**
    * Метод выполняет сортировку таблицы
    * @param {number} column - номер колонки, по которой
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+  this.sort = (column, desc = false) => {
+    sortedRows = Array.from(this.el.rows)
+      .slice(1)
+      .sort((rowA, rowB) => {
+        if (!desc) {
+          if (isFinite(+rowA.cells[column].innerHTML)) {
+            return +rowA.cells[column].innerHTML > +rowB.cells[column].innerHTML ? 1 : -1;
+          } else {
+            return rowA.cells[column].innerHTML > rowB.cells[column].innerHTML ? 1 : -1;
+          }
+        } else {
+          if (isFinite(+rowA.cells[column].innerHTML)) {
+            return +rowA.cells[column].innerHTML > +rowB.cells[column].innerHTML ? -1 : 1;
+          } else {
+            return rowA.cells[column].innerHTML > rowB.cells[column].innerHTML ? -1 : 1;
+          }
+        }
+
+      });
+
+    this.el.tBodies[0].append(...sortedRows);
+  };
 }
