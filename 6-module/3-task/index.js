b013d972e405b3875a1abfc55233236711edc3bb
Vasilii Shabanov <shabspb@gmail.com>
2020-03-03 15:45:07 +0300

    decision 6-module/3-task Учебный проект: компонента Menu

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -5,27 +5,51 @@ class Menu {
   <ul class="list-group sidebar">
     <li class="list-group-item dropdown">
       <a class="nav-link dropdown-toggle" id="cameraphotos">Camera &amp; Photo</a>
-      <ul class="dropdown-menu">   
-        
+      <ul class="dropdown-menu">
+
        <li data-id="cameraphotos_accessories" class="dropdown-item"><a>Accessories</a></li>
-    
+
       </ul>
     </li>
-  
+
     <li class="list-group-item dropdown">
       <a class="nav-link dropdown-toggle" id="cinema">Home Cinema, TV &amp; Video</a>
-      <ul class="dropdown-menu">   
-        
+      <ul class="dropdown-menu">
+
        <li data-id="cinema_audio" class="dropdown-item"><a>Audio</a></li>
-    
+
        <li data-id="cinema_video" class="dropdown-item"><a>Video</a></li>
-    
+
       </ul>
     </li>
   </ul>
   `;
 
   constructor(element) {
+    this.el = element;
+    this.addMenu();
+    this.toggleMenu();
+  }
+
+  addMenu() {
+    this.el.innerHTML = this.template;
+  }
+
+  toggleMenu() {
+    let listGroupItems = this.el.querySelectorAll('.list-group-item'),
+      backdrop = document.querySelector('.backdrop');
+    for (let item of listGroupItems) {
+      item.addEventListener('pointerenter', (event) => {
+        let dropdown = event.target.querySelector('.dropdown-menu');
+        dropdown.classList.toggle('show');
+        backdrop.classList.toggle('show');
+      });
+      item.addEventListener('pointerleave', (event) => {
+        let dropdownMenu = event.target.querySelector('.dropdown-menu');
+        dropdownMenu.classList.toggle('show');
+        backdrop.classList.toggle('show');
+      });
+    }
   }
 }
 
